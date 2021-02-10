69ebd270fac6e03c2e6cacf2c8a3976900154012
nartexyu <70301421+nartexyu@users.noreply.github.com>
2021-02-10 13:41:03 -0800

    added components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+// Imports
+import Header from './components/Header'
+import Main from './components/Main'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap'
 
 function App() {
   return (
     <>
+    <Header />
+    <Main />
     </>
   );
 }
